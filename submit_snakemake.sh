cbfacf7d1ea1c2b8e7a84a2f57aa4fbf47cb1d60
Sophie Hoffman <33765268+shoffm@users.noreply.github.com>
2023-10-30 11:47:23 +0000

    Update submit_snakemake.sh

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -6,5 +6,6 @@
 #BSUB -R "select[mem>25000] rusage[mem=25000] span[hosts=1]"
 #BSUB -M25000
 
+# for running crosscheck with 4000 samples, crosscheck fingerprints job required M330000 and basement queue (254 hours or 10.5 days)
 # Job commands
 snakemake --latency-wait 90 --jobs 50 --cluster "bsub -o /path/to/output/logfiles -M25000 --jobname={rule}_{wildcards}"
